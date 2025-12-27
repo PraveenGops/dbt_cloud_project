b0e48438d6374b0ba6c0c8c2dcec39cdad34eeea
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2025-12-27 22:46:53 +0530

    Update filecheck.sql

--- macros/filecheck.sql
+++ macros/filecheck.sql
@@ -1,11 +1,12 @@
 {% macro filecheck_macro(year,table_name) %}
     {% set result_count %}
-        select count(*)
+        select count(*) as CNT
         from {{ table_name }}
         where right(year,4) = '{{ year }}'
     {% endset %}
 
 {% set result = run_query(query) %}
-{% return(result) %}
+{{ return(result.rows[0]['CNT']) }}
+
 
 {% endmacro %}
